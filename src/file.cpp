ad8b556d8b59490913c6548cd1156d2ab7550fd0
Restyled.io <commits@restyled.io>
2024-01-31 12:00:42 +0000

    Restyled by clang-format

--- src/file.cpp
+++ src/file.cpp
@@ -36,17 +36,16 @@
  * \author Bhaskara Marthi
  */
 
-#include <occupancy_grid_utils/file.h>
-#include <tf/transform_datatypes.h>
 #include <nav_msgs/GetMap.h>
-#include <opencv2/imgproc/imgproc.hpp>
+#include <occupancy_grid_utils/file.h>
 #include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <tf/transform_datatypes.h>
 
-namespace occupancy_grid_utils
-{
+namespace occupancy_grid_utils {
 
-namespace nm=nav_msgs;
-namespace gm=geometry_msgs;
+namespace nm = nav_msgs;
+namespace gm = geometry_msgs;
 
 /************************************************************
  * Implementation
@@ -54,109 +53,102 @@ namespace gm=geometry_msgs;
  * does not export the library in its manifest
  ***********************************************************/
 
-
 #include <cstring>
 #include <stdexcept>
 
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-#include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
 
 #include "LinearMath/btMatrix3x3.h"
 
 // compute linear index for given map coords
 #define MAP_IDX(sx, i, j) ((sx) * (j) + (i))
 
-void
-loadMapFromFile(nav_msgs::GetMap::Response* resp,
-                const char* fname, double res, bool negate,
-                double occ_th, double free_th, double* origin)
-{
-    cv::Mat img;
+void loadMapFromFile(nav_msgs::GetMap::Response *resp, const char *fname,
+                     double res, bool negate, double occ_th, double free_th,
+                     double *origin) {
+  cv::Mat img;
 
-    unsigned int i,j;
-    double occ;
-    double color_avg;
+  unsigned int i, j;
+  double occ;
+  double color_avg;
 
-    // Load the image using OpenCV.  If we get NULL data back, the image load failed.
-    cv::Mat imgColor = cv::imread( fname );
-    if(!imgColor.data)
-    {
-        std::string errmsg = std::string("failed to open image file \"") +
-                             fname + std::string("\"");
-        throw std::runtime_error(errmsg);
+  // Load the image using OpenCV.  If we get NULL data back, the image load
+  // failed.
+  cv::Mat imgColor = cv::imread(fname);
+  if (!imgColor.data) {
+    std::string errmsg =
+        std::string("failed to open image file \"") + fname + std::string("\"");
+    throw std::runtime_error(errmsg);
+  }
+  cv::cvtColor(imgColor, img, cv::COLOR_BGR2GRAY);
+
+  // Copy the image data into the map structure
+  resp->map.info.width = img.rows;
+  resp->map.info.height = img.cols;
+  resp->map.info.resolution = res;
+  resp->map.info.origin.position.x = *(origin);
+  resp->map.info.origin.position.y = *(origin + 1);
+  resp->map.info.origin.position.z = 0.0;
+  btQuaternion q;
+  q.setEuler(*(origin + 2), 0, 0);
+  resp->map.info.origin.orientation.x = q.x();
+  resp->map.info.origin.orientation.y = q.y();
+  resp->map.info.origin.orientation.z = q.z();
+  resp->map.info.origin.orientation.w = q.w();
+
+  // Allocate space to hold the data
+  resp->map.data.resize(resp->map.info.width * resp->map.info.height);
+
+  // Copy pixel data into the map structure
+  for (j = 0; j < resp->map.info.height; j++) {
+    for (i = 0; i < resp->map.info.width; i++) {
+      // Compute mean of RGB for this pixel
+      color_avg = (double)(img.at<uint8_t>(i, j));
+
+      // If negate is true, we consider blacker pixels free, and whiter
+      // pixels free.  Otherwise, it's vice versa.
+      if (negate)
+        occ = color_avg / 255.0;
+      else
+        occ = (255 - color_avg) / 255.0;
+
+      // Apply thresholds to RGB means to determine occupancy values for
+      // map.  Note that we invert the graphics-ordering of the pixels to
+      // produce a map with cell (0,0) in the lower-left corner.
+      if (occ > occ_th)
+        resp->map.data[MAP_IDX(resp->map.info.width, i,
+                               resp->map.info.height - j - 1)] = +100;
+      else if (occ < free_th)
+        resp->map.data[MAP_IDX(resp->map.info.width, i,
+                               resp->map.info.height - j - 1)] = 0;
+      else
+        resp->map.data[MAP_IDX(resp->map.info.width, i,
+                               resp->map.info.height - j - 1)] = -1;
     }
-    cv::cvtColor(imgColor, img, cv::COLOR_BGR2GRAY);
-
-    // Copy the image data into the map structure
-    resp->map.info.width = img.rows;
-    resp->map.info.height = img.cols;
-    resp->map.info.resolution = res;
-    resp->map.info.origin.position.x = *(origin);
-    resp->map.info.origin.position.y = *(origin+1);
-    resp->map.info.origin.position.z = 0.0;
-    btQuaternion q;
-    q.setEuler(*(origin + 2), 0, 0);
-    resp->map.info.origin.orientation.x = q.x();
-    resp->map.info.origin.orientation.y = q.y();
-    resp->map.info.origin.orientation.z = q.z();
-    resp->map.info.origin.orientation.w = q.w();
-
-    // Allocate space to hold the data
-    resp->map.data.resize(resp->map.info.width * resp->map.info.height);
-
-    // Copy pixel data into the map structure
-    for(j = 0; j < resp->map.info.height; j++)
-    {
-        for (i = 0; i < resp->map.info.width; i++)
-        {
-            // Compute mean of RGB for this pixel
-            color_avg = (double)(img.at<uint8_t>(i, j));
-
-            // If negate is true, we consider blacker pixels free, and whiter
-            // pixels free.  Otherwise, it's vice versa.
-            if(negate)
-                occ = color_avg / 255.0;
-            else
-                occ = (255 - color_avg) / 255.0;
-
-            // Apply thresholds to RGB means to determine occupancy values for
-            // map.  Note that we invert the graphics-ordering of the pixels to
-            // produce a map with cell (0,0) in the lower-left corner.
-            if(occ > occ_th)
-                resp->map.data[MAP_IDX(resp->map.info.width,i,resp->map.info.height - j - 1)] = +100;
-            else if(occ < free_th)
-                resp->map.data[MAP_IDX(resp->map.info.width,i,resp->map.info.height - j - 1)] = 0;
-            else
-                resp->map.data[MAP_IDX(resp->map.info.width,i,resp->map.info.height - j - 1)] = -1;
-        }
-    }
+  }
 }
-
 
 /************************************************************
  * Interface
  ***********************************************************/
 
-nm::OccupancyGrid::Ptr loadGrid (const std::string& fname,
-                                 const double resolution,
-                                 const gm::Pose& origin_pose)
-{
-    double origin[3];
-    origin[0] = origin_pose.position.x;
-    origin[1] = origin_pose.position.y;
-    origin[2] = tf::getYaw(origin_pose.orientation);
+nm::OccupancyGrid::Ptr loadGrid(const std::string &fname,
+                                const double resolution,
+                                const gm::Pose &origin_pose) {
+  double origin[3];
+  origin[0] = origin_pose.position.x;
+  origin[1] = origin_pose.position.y;
+  origin[2] = tf::getYaw(origin_pose.orientation);
 
-    nm::GetMap::Response resp;
-    loadMapFromFile(&resp, fname.c_str(), resolution, false,
-                    DEFAULT_OCC_THRESHOLD, DEFAULT_FREE_THRESHOLD,
-                    origin);
-    nm::OccupancyGrid::Ptr grid(new nm::OccupancyGrid(resp.map));
-    return grid;
+  nm::GetMap::Response resp;
+  loadMapFromFile(&resp, fname.c_str(), resolution, false,
+                  DEFAULT_OCC_THRESHOLD, DEFAULT_FREE_THRESHOLD, origin);
+  nm::OccupancyGrid::Ptr grid(new nm::OccupancyGrid(resp.map));
+  return grid;
 }
 
-
-
-} // namespace
+} // namespace occupancy_grid_utils
